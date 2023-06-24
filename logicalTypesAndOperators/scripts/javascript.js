fcdf1b4873986075df281aaf4fcfb7bc5c0ef47f
Roberto Aleydon <aleydonroberto@gmail.com>
2023-06-24 09:42:38 -0300

    Added Arithmetic Operators and Special Numbers

--- logicalTypesAndOperators/scripts/javascript.js
+++ logicalTypesAndOperators/scripts/javascript.js
@@ -3,4 +3,19 @@
 console.log(typeof 13);
 console.log(typeof 1.8);
 console.log(typeof -5);
-console.log(7 + 1.6);
+console.log(typeof 1.6);
+
+// Arithmetic Operators
+
+console.log(6 + 2);
+console.log(6 - 2);
+console.log(6 % 2);
+console.log(6 / 2);
+console.log(6 * 2);
+console.log(6 ** 2);
+
+// Special Numbers
+
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
