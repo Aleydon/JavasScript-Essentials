2483810a64df96fbe4b8d564d52addc7c4feb467
Roberto Aleydon <aleydonroberto@gmail.com>
2023-06-24 10:53:01 -0300

    Added Logical Operators and Updated Numbers, Arithmetic Operators, Strings, Booleans

--- logicalTypesAndOperators/scripts/javascript.js
+++ logicalTypesAndOperators/scripts/javascript.js
@@ -1,5 +1,6 @@
 // Type Numbers
 
+console.log("Numbers");
 console.log(typeof 13);
 console.log(typeof 1.8);
 console.log(typeof -5);
@@ -7,6 +8,7 @@ console.log(typeof 1.6);
 
 // Arithmetic Operators
 
+console.log("\nArithmetic Operators");
 console.log(6 + 2);
 console.log(6 - 2);
 console.log(6 % 2);
@@ -16,14 +18,38 @@ console.log(6 ** 2);
 
 // Special Numbers
 
+console.log("\nSpecial Numbers");
 console.log(typeof NaN);
 console.log(typeof Infinity);
 console.log(typeof -Infinity);
 
 // Strings
+
+console.log("\nStrings");
 console.log(typeof "Roberto Aleydon");
 console.log(typeof `Template Literals`);
 console.log(`Template Literals ${2 + 6}`);
 console.log("'text with quotes'");
 console.log("Break \n Line");
 console.log("Strings", "Concatenation");
+
+// Booleans
+
+console.log("\nBooleans");
+console.log(true);
+console.log(false);
+console.log(5 > 2);
+console.log(5 < 2);
+console.log(5 >= 2);
+console.log(5 <= 2);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(5 !== 5);
+
+// Logical Operators
+
+console.log("\nLogical Operators");
+console.log(5 > 2 && 4 < 6);
+console.log(5 >= 5 || 4 > 6 === 10);
+console.log(!true);
+console.log(!false);
