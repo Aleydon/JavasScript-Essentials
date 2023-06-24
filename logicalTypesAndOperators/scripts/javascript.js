5bfb7b3b527d7761e5c872c4f25ea24256633bc1
Roberto Aleydon <aleydonroberto@gmail.com>
2023-06-24 10:25:24 -0300

    Added Strings

--- logicalTypesAndOperators/scripts/javascript.js
+++ logicalTypesAndOperators/scripts/javascript.js
@@ -19,3 +19,11 @@ console.log(6 ** 2);
 console.log(typeof NaN);
 console.log(typeof Infinity);
 console.log(typeof -Infinity);
+
+// Strings
+console.log(typeof "Roberto Aleydon");
+console.log(typeof `Template Literals`);
+console.log(`Template Literals ${2 + 6}`);
+console.log("'text with quotes'");
+console.log("Break \n Line");
+console.log("Strings", "Concatenation");
