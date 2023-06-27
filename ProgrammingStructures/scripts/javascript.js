e37800fa9256dabb19e6ac78d8556a992ccb96dc
Roberto Aleydon <aleydonroberto@gmail.com>
2023-06-27 19:36:16 -0300

    ADD: Variables and Constants

---
+++ ProgrammingStructures/scripts/javascript.js
@@ -0,0 +1,18 @@
+// Variables and Constants
+
+console.log("\nVariables");
+
+let banana = 5;
+let square = 4,
+  triangle = 3,
+  rectangle = 4;
+
+const myConst = "Constants";
+
+console.log(banana * banana);
+console.log(`You have ${banana} bananas`);
+console.log(square * (triangle + rectangle));
+console.log(`${myConst} do not allow the value to be changed`);
+
+banana = "banana";
+console.log(`Do you like ${banana}?`);
