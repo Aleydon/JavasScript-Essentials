779c84ccddfc85fc3303b14318dd5b92e57e5bcb
Roberto Aleydon <aleydonroberto@gmail.com>
2023-06-28 19:43:28 -0300

    Updated Constant Name

--- ProgrammingStructures/scripts/javascript.js
+++ ProgrammingStructures/scripts/javascript.js
@@ -7,12 +7,12 @@ let square = 4,
   triangle = 3,
   rectangle = 4;
 
-const myConst = "Constants";
+const constants = "Constants";
 
 console.log(banana * banana);
 console.log(`You have ${banana} bananas`);
 console.log(square * (triangle + rectangle));
-console.log(`${myConst} do not allow the value to be changed`);
+console.log(`${constants} do not allow the value to be changed`);
 
 banana = "banana";
 console.log(`Do you like ${banana}?`);
