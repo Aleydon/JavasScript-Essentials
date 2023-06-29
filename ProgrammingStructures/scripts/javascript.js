f0c5c4f4fe7fbfffd8fce93d1005d1c830b2a27f
Aleydon <aleydonroberto@gmail.com>
2023-06-28 21:24:20 -0300

    ADD: Functions + Functions Built-In

--- ProgrammingStructures/scripts/javascript.js
+++ ProgrammingStructures/scripts/javascript.js
@@ -16,3 +16,29 @@ console.log(`${constants} do not allow the value to be changed`);
 
 banana = "banana";
 console.log(`Do you like ${banana}?`);
+
+// Functions
+
+console.log("\nFunctions");
+
+function functionWithoutParam() {
+  return "Without parameter";
+}
+function functionWithParams(param1, param2) {
+  return param1 + param2;
+}
+
+console.log(functionWithoutParam());
+console.log(functionWithParams("JavaScript", "Essentials"));
+
+let age = prompt("how old are you?");
+console.log(`Functions  Built-In ${age}`);
+
+alert("Function Built-In 2");
+
+let higherNumber = Math.max(2, 5, 1, 9);
+console.log(
+  `Function Built-In 3\n(2, 5, 1, 9): Higher Number = ${higherNumber}`
+);
+
+console.log("console.log is also a built-in function");
