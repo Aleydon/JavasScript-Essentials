d2c8b89351952a9d3e1f3e5053e3381739fad299
Aleydon <aleydonroberto@gmail.com>
2023-07-01 15:43:37 -0300

    ADD: else if

---
+++ ControlStructures/elseif/scripts/javascript.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 3;
+
+if (a + b === 3) {
+  console.log("The result is 3");
+} else if (a === 4) {
+  console.log("The value of a is 4");
+} else if (b === 3) {
+  console.log("The value of b is 3");
+} else {
+  console.log("No condition above");
+}
