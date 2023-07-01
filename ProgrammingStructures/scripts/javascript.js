b131990d3721451d069198441b485feafb4b0a7d
Aleydon <aleydonroberto@gmail.com>
2023-07-01 15:24:59 -0300

    hue

--- ProgrammingStructures/scripts/javascript.js
+++ ProgrammingStructures/scripts/javascript.js
@@ -42,3 +42,23 @@ console.log(
 );
 
 console.log("console.log is also a built-in function");
+
+// Control Structures
+
+console.log("\nControl Structures");
+
+let myName = "Roberto Aleydon";
+let numberA = 10;
+let numberB = 7;
+
+if (myName === "Roberto Aleydon") {
+  console.log("You are Roberto Aleydon!");
+} else {
+  console.log("You are not Robert Aleydon!");
+}
+
+if (numberA <= numberB) {
+  console.log("NumberA is less than NumberB");
+} else if (numberA >= numberB) {
+  console.log("NumberA is greater than NumberB");
+}
