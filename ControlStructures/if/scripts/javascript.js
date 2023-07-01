9d15bb75c4cdba6944a0b469419d411171945764
Aleydon <aleydonroberto@gmail.com>
2023-07-01 12:03:25 -0300

    Created ControlStructures and  ADD:  If

---
+++ ControlStructures/if/scripts/javascript.js
@@ -0,0 +1,13 @@
+let number1 = 7
+let number2 = 5
+
+if (number1 === number2) {
+    console.log('Number1 is the same as Number2');
+}
+
+if (number1 > number2) {
+    console.log("Number1 is greater than Number2");
+}
+if (number1 < number2) {
+    console.log("Number1 is less than Number2");
+}
