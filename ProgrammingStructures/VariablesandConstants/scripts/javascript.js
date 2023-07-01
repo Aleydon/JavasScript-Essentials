7bf5db14f970225d3870187ff4027f3cd00ae40a
Aleydon <aleydonroberto@gmail.com>
2023-07-01 11:49:05 -0300

    Structural Decoupling

---
+++ ProgrammingStructures/VariablesandConstants/scripts/javascript.js
@@ -0,0 +1,18 @@
+// Variables and Constants
+
+console.log("\nVariables");
+
+let banana = 5;
+let square = 4,
+  triangle = 3,
+  rectangle = 4;
+
+const constants = "Constants";
+
+console.log(banana * banana);
+console.log(`You have ${banana} bananas`);
+console.log(square * (triangle + rectangle));
+console.log(`${constants} do not allow the value to be changed`);
+
+banana = "banana";
+console.log(`Do you like ${banana}?`);
