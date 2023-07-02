f41d17993c3fcb5c97366d43c6fbc907bb9e714e
Aleydon <aleydonroberto@gmail.com>
2023-07-02 16:58:42 -0300

    ADD: else if

---
+++ ControlStructures/elseIf/scripts/javascript.js
@@ -0,0 +1,12 @@
+let numberA = 5;
+let numberB = 3;
+
+if (numberA === 3) {
+  console.log("NumberA is 3");
+} else if (numberB === 4) {
+  console.log("NumberB is 4");
+} else if (numberA >= numberB) {
+  console.log("NumberA is greater than NumberB");
+} else {
+  console.log("No condition above");
+}
