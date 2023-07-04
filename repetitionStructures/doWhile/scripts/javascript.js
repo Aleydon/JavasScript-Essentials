1066a7dc7b36d4c1bbcdc391207a7c202d73fda7
Aleydon <aleydonroberto@gmail.com>
2023-07-03 21:13:00 -0300

    ADD: do while

---
+++ repetitionStructures/doWhile/scripts/javascript.js
@@ -0,0 +1,16 @@
+// Do While Structure
+
+let x = 30;
+let y = 0;
+
+do {
+  x -= 5;
+  console.log(`X: ${x}`);
+} while (x > 0);
+
+console.log("Do While\n");
+
+do {
+  y += 1;
+  console.log(`Y: ${y}`);
+} while (y < 10);
