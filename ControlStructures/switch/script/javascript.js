c641538a412312d50e2d7c70c457addfcd74d8b9
Aleydon <aleydonroberto@gmail.com>
2023-07-06 19:56:40 -0300

    ADD: switch case

---
+++ ControlStructures/switch/script/javascript.js
@@ -0,0 +1,15 @@
+let firstName = prompt("what is your name?");
+
+switch (firstName) {
+  case "Roberto":
+    console.log(`Welcome ${firstName}`);
+    break;
+
+  case "Julia":
+    console.log(`Welcome ${firstName}`);
+    break;
+
+  default:
+    console.log("Unrecognized Name");
+    break;
+}
