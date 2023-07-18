c6e715a0cdeaac04d5aad3e7700c6b656e4a2d43
Aleydon <aleydonroberto@gmail.com>
2023-07-18 20:35:24 -0300

    ADD: Getting last index

--- arraysAndObjects/arrays/script/javascript.js
+++ arraysAndObjects/arrays/script/javascript.js
@@ -1,6 +1,6 @@
 // Arrays
 
-console.log("Arrays \n");
+console.log("Arrays: \n");
 
 let numbers = [1, 2, 3, 4];
 let strings = ["Aleydon", "Julia", "Beatriz"];
@@ -11,3 +11,16 @@ console.log(numbers);
 console.log(strings);
 console.log(booleans);
 console.log(mixedTypes);
+
+// Getting element from array
+
+console.log("\nGetting element from array:");
+
+console.log(numbers[3]);
+console.log(strings[0]);
+console.log(booleans[2]);
+console.log(mixedTypes[1]);
+
+// Last index
+console.log("\nlast index:");
+console.log(strings[strings.length - 1]);
