82595d6ca9387ceaf15b8fca36d2c8749f2fa89f
Aleydon <aleydonroberto@gmail.com>
2023-07-18 20:23:22 -0300

    ADD: arrays

---
+++ arraysAndObjects/arrays/script/javascript.js
@@ -0,0 +1,13 @@
+// Arrays
+
+console.log("Arrays \n");
+
+let numbers = [1, 2, 3, 4];
+let strings = ["Aleydon", "Julia", "Beatriz"];
+let booleans = [true, false, true];
+let mixedTypes = [1, "Javascript", true, "Essentials"];
+
+console.log(numbers);
+console.log(strings);
+console.log(booleans);
+console.log(mixedTypes);
