386fe95f9344ae0ccf8c5fe515aa6317176382ae
Aleydon <aleydonroberto@gmail.com>
2023-07-22 16:18:32 -0300

    ADD: Loops in Arrays

---
+++ arraysAndObjects/loopsInArrays/scripts/javascript.js
@@ -0,0 +1,12 @@
+let names = [
+  "Roberto Aleydon",
+  "Ana Julia",
+  "Beatriz Alves",
+  "Julie Christine",
+  "Lanny Oliveira",
+];
+
+for (let i = 0; i <= names.length; i++) {
+  const element = names[i];
+  console.log(element);
+}
