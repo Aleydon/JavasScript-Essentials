4c3aceeb673cbba824f31d21dece37204f1d40b1
Aleydon <aleydonroberto@gmail.com>
2023-07-23 16:25:36 -0300

    ADD: Push and Pop

---
+++ arraysMethods/pushAndPop/scripts/javascript.js
@@ -0,0 +1,12 @@
+let peoples = ["Ana", "Maria", "Bianca", "Felipe", "João"];
+
+console.log(peoples);
+
+const removeLastIndex = peoples.pop();
+
+console.log(`Pop remove last index of array: ${removeLastIndex}`);
+
+const addItem = peoples.push("Fernanda");
+
+console.log(`Push adds a new item to the array: ${addItem}`);
+console.log(peoples);
