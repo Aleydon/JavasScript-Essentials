88777ea3ac8744d178c293c6b482e386a1eed86e
Aleydon <aleydonroberto@gmail.com>
2023-07-25 19:39:31 -0300

    ADD: slice

---
+++ arraysMethods/slice/scripts/javascript.js
@@ -0,0 +1,5 @@
+let users = ["Matheus", "Fabiana", "Rodrigo", "Heather", "Chun-li"];
+
+console.log(`Get index ${users.slice(2)} to ...`);
+console.log(`Get index ${users.slice(1, 3)}:  1 to 3 - 1`);
+console.log(`Get reverse index ${users.slice(-3)}`);
