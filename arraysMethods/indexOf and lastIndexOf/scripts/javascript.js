42e99801f8e449a30f3810cb618754fc0de9e71e
Aleydon <aleydonroberto@gmail.com>
2023-07-25 19:21:37 -0300

    ADD: indexOf and lastIndexOf

---
+++ arraysMethods/indexOf and lastIndexOf/scripts/javascript.js
@@ -0,0 +1,13 @@
+let cars = [
+  "Mitsubishi Eclipse",
+  "Nyssan Skyline GTR",
+  "Madza RX8",
+  "Toyota Supra",
+  "Madza RX8",
+  "Nyssan Skyline GTR",
+];
+
+console.log(`Index of Skylne is: ${cars.indexOf("Nyssan Skyline GTR")}`);
+console.log(
+  `Last index of Skyline is: ${cars.lastIndexOf("Nyssan Skyline GTR")}`
+);
