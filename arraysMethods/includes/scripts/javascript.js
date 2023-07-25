217be6388ec519605709007e301c8a9a7943fdf1
Aleydon <aleydonroberto@gmail.com>
2023-07-25 20:08:33 -0300

    ADD: includes

---
+++ arraysMethods/includes/scripts/javascript.js
@@ -0,0 +1,9 @@
+// Includes: Checks if the array has any elements
+
+let cars = ["BMW", "Fiat", "Audi", "Madza", "Nyssan"];
+
+if (cars.includes("BMW")) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
