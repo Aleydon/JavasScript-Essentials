95afec5decdc1f1e268da6b0e4332c4428f62e8e
Aleydon <aleydonroberto@gmail.com>
2023-07-27 20:02:40 -0300

    ADD: padStart

---
+++ stringsMethods/padStart/scripts/javascript.js
@@ -0,0 +1,11 @@
+// Add characters before string
+
+let reverseNumber = "0";
+
+console.log(reverseNumber.padStart(5, "1"));
+
+let numbers = ["1", "2", "3", "4", "5"];
+
+numbers.forEach((item) => {
+  console.log(item.padStart(2, "0"));
+});
