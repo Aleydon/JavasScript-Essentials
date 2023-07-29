fe6c7526585baec129eb4e9402abaef1f5a61487
Aleydon <aleydonroberto@gmail.com>
2023-07-29 13:03:26 -0300

    ADD: split

---
+++ stringsMethods/split/scripts/javascript.js
@@ -0,0 +1,8 @@
+// Split return an Array
+
+let phrase = "My name is Roberto Aleydon";
+let products = "Xiaomi, Samsung, LG, Motorola";
+
+// Returns an array with spaces
+console.log(phrase.split(""));
+console.log(products.split(","));
