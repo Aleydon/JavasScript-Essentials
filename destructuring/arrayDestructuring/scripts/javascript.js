5b44bee3a4b1a663c6ebf7bf4b0314247e9eed8d
Aleydon <aleydonroberto@gmail.com>
2023-08-03 22:03:16 -0300

    ADD: Array  Destructuring

---
+++ destructuring/arrayDestructuring/scripts/javascript.js
@@ -0,0 +1,7 @@
+// Create variables with the value of the array
+
+let names = ["Bianca", "Marcia", "Tadeu", "Guilherme"];
+
+let [name1, name2, name3, name4] = names;
+
+console.log(`Names: ${name1}, ${name2}, ${name3}, ${name4}`);
